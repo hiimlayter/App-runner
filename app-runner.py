76b2365ef59fcd17917f0aebb8430c2f0a91f0d9
hiimlayter <maniaczek12300@gmail.com>
2020-02-26 14:05:47 +0100

    Switch to Atom

--- app-runner.py
+++ app-runner.py
@@ -1,20 +1,21 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog
 import os
 import ntpath
 ntpath.basename("a/b/c")
 
 root = tk.Tk()
 root.title("App Runner")
-root.resizable(width=False,height=False)
+root.resizable(width=False, height=False)
 sets = []
 
 activeSetApps = []
 selectedApp = ""
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - FUNCTIONS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - -FUNCTIONS - - - - - - - - - - - - - - - -
 
-#Function for set reading
+# Function for set reading
+
 
 def setRead():
     if os.path.isdir('sets'):
@@ -24,13 +25,15 @@ def setRead():
     else:
         os.mkdir("sets")
 
-#Functions for getting name of file
+# Functions for getting name of file
+
 
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
-#Function for list element selection
+# Function for list element selection
+
 
 def selectSet(evt):
     activeSet = str(setsList.get(setsList.curselection()))
@@ -38,6 +41,7 @@ def selectSet(evt):
     activeSetApps = f.read().split('\n')
     print(activeSetApps)
     f.close()
+
 
 def selectApp(evt):
 
@@ -48,80 +52,90 @@ def selectApp(evt):
     f.close()
 
 
-#BUTTTONS
+# BUTTTONS
 
-#Function for adding apps to set
+# Function for adding apps to set
 
 def addApp():
 
     appClear()
 
-    filename = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables","*.exe"), ("all files", "*.*")))
+    filename = filedialog.askopenfilename(
+        initialdir="/", title="Select File", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
     if filename != "":
         activeSetApps.append(filename)
 
     printApps()
 
-#Function for removing apps from set
+# Function for removing apps from set
+
 
 def removeApp():
 
     appClear()
-    #...
+    # ...
     printSets()
 
-#Function fot running selected app set
+# Function fot running selected app set
+
 
 def runApps():
     for app in activeSetApps:
         os.startfile(app)
 
-#Function for removing set
+# Function for removing set
+
 
 def delSet():
-    with open('save.txt','w') as f:
+    with open('save.txt', 'w') as f:
         for app in apps:
             f.write(app+'\n')
     printSets()
 
-#Function for adding new set
+# Function for adding new set
+
 
 def addSet():
     print("Add Set")
     printSets()
 
-#LISTING
+# LISTING
 
-#Function for printing list of sets
+# Function for printing list of sets
+
 
 def printSets():
     setClear()
     for set in sets:
-        setsList.insert(tk.END, set.replace('.txt',''))
+        setsList.insert(tk.END, set.replace('.txt', ''))
 
-#Function for printing list of apps
+# Function for printing list of apps
+
 
 def printApps():
     appClear()
     for app in activeSetApps:
         appList.insert(tk.END, path_leaf(app))
 
-#Function for lists clearing
+# Function for lists clearing
+
 
 def setClear():
-    setsList.delete(0,'end')
+    setsList.delete(0, 'end')
+
 
 def appClear():
     appList.delete(0, 'end')
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - GUI - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-#Canvas
+# Canvas
+
 
 canvas = tk.Canvas(root, height=700, width=700)
 canvas.pack()
 
-#Frames
+# Frames
 
 appSetList = tk.Frame(root)
 appSetOptions = tk.Frame(root)
@@ -133,55 +147,55 @@ appSetList.place(relwidth=0.4, relheight=0.4, relx=0.55, rely=0.05)
 appsInSet.place(relwidth=0.9, relheight=0.3, relx=0.05, rely=0.5)
 appsOptions.place(relwidth=0.9, relheight=0.2, relx=0.05, rely=0.85)
 
-#Labels
+# abels
 
-setsListLabel = tk.Label(appSetList,text="Saved sets:")
-appsListLabel = tk.Label(appsInSet,text="Apps to start in set:")
+setsListLabel = tk.Label(appSetList, text="Saved sets:")
+appsListLabel = tk.Label(appsInSet, text="Apps to start in set:")
 
-setsListLabel.place(relwidth=1,relheight=0.1, rely=0)
-appsListLabel.place(relwidth=1,relheight=0.1, rely=0)
+setsListLabel.place(relwidth=1, relheight=0.1, rely=0)
+appsListLabel.place(relwidth=1, relheight=0.1, rely=0)
 
-#ListBoxes
+# ListBoxes
 
 setsList = tk.Listbox(appSetList)
 appList = tk.Listbox(appsInSet)
 
-setsList.bind('<<ListboxSelect>>',selectSet)
-appList.bind('<<ListboxSelect>>',selectApp)
+setsList.bind('<<ListboxSelect>>', selectSet)
+appList.bind('<<ListboxSelect>>', selectApp)
 
-setsList.place(relwidth=1,relheight=0.9, rely=0.1)
-appList.place(relwidth=1,relheight=0.9, rely=0.1)
+setsList.place(relwidth=1, relheight=0.9, rely=0.1)
+appList.place(relwidth=1, relheight=0.9, rely=0.1)
 
-#Buttons
+# Buttons
 
-runSet = tk.Button(appSetOptions,text="Run Set",
-                    padx=5, pady=2,
-                    fg="white", bg="#0F0F0F",
-                    height = 2, width = 30, command=runApps)
-runSet.place(relx=0.15,rely=0.2)
+runSet = tk.Button(appSetOptions, text="Run Set",
+                   padx=5, pady=2,
+                   fg="white", bg="#0F0F0F",
+                   height=2, width=30, command=runApps)
+runSet.place(relx=0.15, rely=0.2)
 
-addSet = tk.Button(appSetOptions,text="Add Set",
-                     padx=5, pady=2,
-                     fg="white", bg="#0F0F0F",
-                     height = 2, width = 30, command=addSet)
-addSet.place(relx=0.15,rely=0.5)
+addSet = tk.Button(appSetOptions, text="Add Set",
+                   padx=5, pady=2,
+                   fg="white", bg="#0F0F0F",
+                   height=2, width=30, command=addSet)
+addSet.place(relx=0.15, rely=0.5)
 
-delSet = tk.Button(appSetOptions,text="Delete Set",
-                    padx=5, pady=2,
-                    fg="white", bg="#0F0F0F",
-                    height = 2, width = 30, command=delSet)
-delSet.place(relx=0.15,rely=0.8)
+delSet = tk.Button(appSetOptions, text="Delete Set",
+                   padx=5, pady=2,
+                   fg="white", bg="#0F0F0F",
+                   height=2, width=30, command=delSet)
+delSet.place(relx=0.15, rely=0.8)
 
-addApp = tk.Button(appsOptions,text="Add App",
-                    padx=5, pady=2,
-                    fg="white", bg="#0F0F0F",
-                    height = 2, width = 30, command=addApp)
+addApp = tk.Button(appsOptions, text="Add App",
+                   padx=5, pady=2,
+                   fg="white", bg="#0F0F0F",
+                   height=2, width=30, command=addApp)
 addApp.place(relx=0.05)
 
-removeApp = tk.Button(appsOptions,text="Remove App",
-                    padx=5, pady=2,
-                    fg="white", bg="#0F0F0F",
-                    height = 2, width = 30, command=removeApp)
+removeApp = tk.Button(appsOptions, text="Remove App",
+                      padx=5, pady=2,
+                      fg="white", bg="#0F0F0F",
+                      height=2, width=30, command=removeApp)
 removeApp.place(relx=0.6)
 
 
@@ -190,4 +204,3 @@ printSets()
 printApps()
 
 root.mainloop()
-
